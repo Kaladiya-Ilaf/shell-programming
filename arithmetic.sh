1994ce2785cf2823fba14e3f3346737693440dd2
Kaladiya-Ilaf <ilafkaladiya03@gmail.com>
2020-07-03 17:47:33 +0530

    Refactoring the program for performing arithmetic operation on input

--- arithmetic.sh
+++ arithmetic.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+#!/bin/bash -x
 
 read -p "Enter first value :" a
 read -p "Enter second value :" b
 read -p "Enter third value :" c
 
-echo "First value : "$a
-echo "Second value : "$b
-echo "Third value : "$c
 
-compute=$(($a+$b*$c))
+Result["compute1"]=`echo $a $b $c | awk '{ printf "%.0f", $1 + $2 * $3 }'`
+Result["compute2"]=`echo $a $b $c | awk '{ printf "%.0f", $1 * $2 + $3 }'`
+Result["compute3"]=`echo $a $b $c | awk '{ printf "%.0f", $3 + $1 / $2 }'`
+Result["compute4"]=`echo $a $b $c | awk '{ printf "%.0f", $1 % $2 + $3 }'`
 
-echo "Result is "$compute
+
