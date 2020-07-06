f1eea693076d6d14c51481ec7c7c852b9dc0ee0d
Kaladiya-Ilaf <ilafkaladiya03@gmail.com>
2020-07-06 17:00:53 +0530

    Email validated for user registration using regular expression and program refactored

--- pattern/userRegistration.sh
+++ pattern/userRegistration.sh
@@ -11,6 +11,7 @@ function validateUser(){
 }
 
 Name="^[A-Z][a-z]{2,}$"
+emailId="^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})*$"
 echo "Welcome For Registration!!!"
 
 read -p "First Name: " fname
@@ -18,3 +19,6 @@ validateUser $fname $Name
 
 read -p "Last Name: " lname
 validateUser $lname $Name
+
+read -p "E-mail: " email
+validateUser $email $emailId
