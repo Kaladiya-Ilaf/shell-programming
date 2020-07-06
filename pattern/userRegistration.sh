049c8c0e576f568ee360e0d932eb8f6b7528e25d
Kaladiya-Ilaf <ilafkaladiya03@gmail.com>
2020-07-06 18:49:00 +0530

    Mobile number validated for user registration using regular expression and program refactored

--- pattern/userRegistration.sh
+++ pattern/userRegistration.sh
@@ -1,5 +1,8 @@
-#!/bin/bash
+#!/bin/bash 
+
 function validateUser(){
+	echo $1
+	echo $2
 	userInput=$1
 	pat=$2
 	if [[ $userInput =~ $pat ]]
@@ -12,6 +15,8 @@ function validateUser(){
 
 Name="^[A-Z][a-z]{2,}$"
 emailId="^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})*$"
+mobileFormat="^91[0-9]{10}$"
+
 echo "Welcome For Registration!!!"
 
 read -p "First Name: " fname
@@ -22,3 +27,6 @@ validateUser $lname $Name
 
 read -p "E-mail: " email
 validateUser $email $emailId
+
+read -p "Mobile: " mobile
+validateUser $mobile $mobileFormat
